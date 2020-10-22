1f04948c90f0cffb80acf37973a84d0551f24779
phellipbonavigo <37884906+phellipbonavigo@users.noreply.github.com>
2020-10-22 18:00:03 -0300

    Add files via upload

---
+++ exercicio1.hs
@@ -0,0 +1,8 @@
+-- Exercício 1
+-- Defina uma função para calcular o quadrado do dobro do seu argumento
+
+quadradoDobro num = (2*num)^2
+
+
+
+
---
+++ exercicio2.hs
@@ -0,0 +1,5 @@
+-- Exercício 2 
+-- . Defina uma função para calcular o dobro do quadrado do seu argumento.
+
+dobroQuadrado x = 2 * x^2
+
